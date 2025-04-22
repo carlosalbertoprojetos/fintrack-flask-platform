781948cd049d95b3890fd3db6786d289bfb6c4e0
carlos_alberto <carlosalbertoprojetos2020@gmail.com>
2025-04-22 07:37:11 -0300

    melhorias layout cards receita, despesa e saldo

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import os
 from app import create_app, db
 from app.models import Category, PaymentMethod, Expense
+from flask import Flask, request, abort
 from flask_migrate import Migrate
 
 app = create_app()
@@ -18,6 +19,17 @@ def init_db():
     db.create_all()
     print("Banco de dados inicializado.")
 
+
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        abort(500, 'Servidor não está rodando com o Werkzeug')
+    func()
+
+@app.route('/shutdown')
+def shutdown():
+    shutdown_server()
+    return 'Servidor encerrado com sucesso.'
 
 if __name__ == "__main__":
     app.run(debug=True)
